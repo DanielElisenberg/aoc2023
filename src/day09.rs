1d3fe31c2bff2dfb2ff0b8355b8c4789e1c8fb7f
DanielElisenberg <Daniel@Elisenberg.com>
2023-12-09 11:45:22 +0100

    day09: refactor

--- src/day09.rs
+++ src/day09.rs
@@ -17,16 +17,13 @@ fn solve_part_one() -> i32 {
         .map(|line| {
             let mut next_line = line.clone();
             let mut prediction = next_line[next_line.len() - 1];
-            loop {
-                let differences = next_line
+            while !next_line.clone().into_iter().all(|d| d == 0) {
+                next_line = next_line
+                    .clone()
                     .windows(2)
                     .map(|w| w[1] - w[0])
                     .collect::<Vec<i32>>();
-                next_line = differences.clone();
                 prediction = prediction + next_line[next_line.len() - 1];
-                if differences.into_iter().all(|d| d == 0) {
-                    break;
-                }
             }
             prediction
         })
@@ -39,16 +36,13 @@ fn solve_part_two() -> i32 {
         .map(|line| {
             let mut next_line = line.clone();
             let mut history = Vec::from([next_line[0]]);
-            loop {
-                let differences = next_line
+            while !next_line.clone().into_iter().all(|d| d == 0) {
+                next_line = next_line
+                    .clone()
                     .windows(2)
                     .map(|w| w[1] - w[0])
                     .collect::<Vec<i32>>();
-                next_line = differences.clone();
                 history.push(next_line[0]);
-                if differences.into_iter().all(|d| d == 0) {
-                    break;
-                }
             }
             let mut prediction = 0;
             (0..(history.len() - 1))
